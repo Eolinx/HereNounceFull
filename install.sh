793f335f5d9a0a08a596966a77ad93f0c9e9d3bc
Alex Furnica <saniafd93@gmail.com>
2023-09-08 03:10:03 +0300

    upgrade

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 echo "Cloning HereNounce..."
 git clone https://github.com/Eolinx/HereNounce.git
+cp ./HereNounce/.devops/docker/filesystem/app/loader.php ./HereNounce/loader.php
 
 echo "Cloning Helix..."
 git clone https://github.com/Eolinx/Helix.git
+cp ./Helix/.devops/docker/filesystem/app/loader.php ./Helix/loader.php
 
+cp .env-example .env
+docker-compose --env-file .env up 
