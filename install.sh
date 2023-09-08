27a3a143f15ba677f009f3ea4b84df652c07c98b
Alex Furnica <saniafd93@gmail.com>
2023-09-08 07:19:22 +0300

    upgrade

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+echo "HereNounce auto install"
+read -p "Enter current node hostname: " hostname
+
+export HERENOUNCE_HOSTNAME=hostname
+
 echo "Cloning HereNounce..."
 git clone https://github.com/Eolinx/HereNounce.git
 cp -r ./HereNounce/.devops/docker/filesystem/app/. ./HereNounce/
@@ -6,9 +12,16 @@ echo "Cloning Helix..."
 git clone https://github.com/Eolinx/Helix.git
 cp -r ./Helix/.devops/docker/filesystem/app/. ./Helix/
 
-cp .env-example .env
+echo "Starting Docker containers"
+#cp .env-example .env
 docker-compose up -d
 #docker-compose --env-file=./.env up 
 
 docker exec -t herenouncefull_herenounce_1 /bin/bash -c "cd /app && php /root/composer.phar install"
 docker exec -t herenouncefull_helix_1 /bin/bash -c "cd /app && php /root/composer.phar install"
+
+echo "Herenounce application install"
+docker exec -it herenouncefull_herenounce_1 /bin/bash -c "cd /app && php ./index.php install -e"
+
+echo "Herenounce Helix install"
+docker exec -it herenouncefull_helix_1 /bin/bash -c "cd /app && php ./index.php install -e"
