14a3e9ef3c73a0bdb5813423d544e754ed2d92dc
Alex Furnica <saniafd93@gmail.com>
2023-09-08 05:49:56 +0300

    upgrade

--- install.sh
+++ install.sh
@@ -7,5 +7,8 @@ git clone https://github.com/Eolinx/Helix.git
 cp -r ./Helix/.devops/docker/filesystem/app/. ./Helix/
 
 cp .env-example .env
-docker-compose up
+docker-compose up -d
 #docker-compose --env-file=./.env up 
+
+docker exec -t herenouncefull_herenounce_1 /bin/bash -c "cd /app && php /root/composer.phar install"
+docker exec -t herenouncefull_helix_1 /bin/bash -c "cd /app && php /root/composer.phar install"
